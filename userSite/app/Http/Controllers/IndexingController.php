a27bf62c90f6c6662e08317c2ff156a047849cc7
Arturo Mantinetti <amantinetti@gmail.com>
2017-04-30 04:25:27 -0300

    insertIndex

--- userSite/app/Http/Controllers/IndexingController.php
+++ userSite/app/Http/Controllers/IndexingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\RevIndex;
 use App\SpaceObject;
 use App\SendObj;
@@ -10,8 +11,21 @@ use App\SendObj;
 class IndexingController extends Controller{
 
 
-    public function search(){
+    public function search($string){
+        $arr = explode(" ",$string);
 
+    }
+
+
+    public function insertIndex($array, $object_id){
+        DB::table('rev_indices')->where('space_object_id', $object_id)->delete();
+        for($i=0;i<count($array);$i++){
+            RevIndex::create([
+                'word' => $array[$i],
+                'pos' => $i,
+                'space_object_id' => $object_id,
+            ]);
+        }
     }
 
 }
