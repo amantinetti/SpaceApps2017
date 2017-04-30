23d93df6d77e0e4c559f9e935dd54738c667ae63
Arturo Mantinetti <amantinetti@gmail.com>
2017-04-30 01:58:15 -0300

    Joako Codes

--- Codes/Ranking.php
+++ Codes/Ranking.php
@@ -1,11 +1,25 @@
 <?php
+
 function Algoritm($objeto){
+
+  $mod = false;
   if($puntos = DB::Table('page_rank')->where('page',$objeto->page)->value('points')){
     if($id = DB::Table('objeto')->where('name',objeto->name)->value('id'){
       foreach ($objeto->properties=>$prop) {
+        $value=$objeto->value;
+        $propid=DB::Table('properties')->where('name',$prop['name'])->value('id')
         if(DB::Table('properties')->where('name',$prop['name'])->value('name')){
+          $pointbase = ObjProp::where([
+            [ 'space_object_id', $id],
+            [ 'property_id', $propid]]);
+          if($puntos>$pointbase->points){
+            $pointbase->points = $puntos;
+            $pointbase->value= $value;
+            $pointbase->save();
+            mod = true;
+            }
+          }
 
-        }
         else{
           $newprop = new Property();
           $newprop->name = $prop['name'];
@@ -17,5 +31,16 @@ function Algoritm($objeto){
           $newvalue->space_object_id=$id;
           $newValue->value=$prop['value'];
           $newValue->points=$puntos;
-          $newValue->save();
+          $newValue->save()
+          mod = true;
         }
+      }
+
+
+    }
+  }
+  if(mod==true){
+    return $id;
+  }
+  return -1;
+}
