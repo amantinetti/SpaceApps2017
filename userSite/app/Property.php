0af511b9d7352d691266215d9d1770e05ec344f7
Arturo Mantinetti <amantinetti@gmail.com>
2017-04-30 03:13:54 -0300

    update

--- userSite/app/Property.php
+++ userSite/app/Property.php
@@ -9,4 +9,8 @@ class Property extends Model{
     protected $fillable = [
         'id', 'name', 'detail', 'if_unique', 'points'
     ];
+
+    public function ObjProps(){
+        return $this->hasMany('App\ObjProp','property_id','id');
+    }
 }
