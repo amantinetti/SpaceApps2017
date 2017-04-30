b49e132365f0537c90e974b33fdf55682816dba9
Ezaah <manueltobar13@gmail.com>
2017-04-30 08:56:08 -0300

    Search Engine

--- userSite/app/Http/Controllers/IndexingController.php
+++ userSite/app/Http/Controllers/IndexingController.php
@@ -11,12 +11,84 @@ use App\Http\Controllers\InputRankingController;
 
 class IndexingController extends Controller{
 
+    public function perfect($indexes, $words){
+        $size = count($words);
+        $lookup = collect($indexes)->groupBy('space_object_id')->toArray();
+        foreach ($lookup as $id) {
+            $jumps = 0;
+            if(count($id) < $size) {
+                continue;
+
+            }
+            $actual_pos = $id[0]['pos'];
+            foreach ($id as $index){
+                if($actual_pos == $index['pos']) {
+                    $actual_pos = $index['pos'];
+                }else if($actual_pos+1 == $index['pos']) {
+                    $actual_pos++;
+                    $jumps++;
+                }else {
+                    break;
+                }
+                if($jumps == $size-1)
+                    return $index['space_object_id'];
+
+            }
+
+        }
+        return -1;
+
+    }
+    public function notPerfect($indexes, $words){
+        $size = count($words);
+        $lookup = collect($indexes)->groupBy('space_object_id')->toArray();
+        $list = array();
+        foreach ($lookup as $id) {
+            $jumps = 0;
+
+            $actual_pos = $id[0]['pos'];
+            foreach ($id as $index){
+                if($actual_pos == $index['pos']) {
+                    continue;
+                }else if($actual_pos+1 == $index['pos']) {
+                    $actual_pos++;
+                    $jumps++;
+                }else {
+                    break;
+                }
+            }
+            array_push($list, [
+                'space_object_id' => $id[0]['space_object_id'],
+                'matches' => $jumps
+            ]);
+
+        }
+        $list = collect($list)->sortByDesc('matches')->toArray();
+        return $list;
+
+    }
 
     public function search($string){
-        $arr = explode(" ",$string);
-        for($i=0;i<count($arr);$i++){
-            $index = RevIndex::find('word',$arr[$i]);
+        $arr = explode(' ',strtolower($string));
+        $indexes = array();
+        foreach ($arr as $word){
+            $index = DB::table('rev_indices')->where('word', $word)->get();
+//            $word_array = array();
+            foreach ($index as $subindex) {
+                array_push($indexes, [
+                    'space_object_id' => $subindex->space_object_id,
+                    'pos' => $subindex->pos
+                ]);
+            }
+
         }
+
+        $id = $this->perfect($indexes,$arr);
+        if($id > 0)
+            return SpaceObject::find($id);
+        $list = $this->notPerfect($indexes, $arr);
+
+        return $list;
     }
 
     public function runIndex($inputObj){
@@ -36,22 +108,22 @@ class IndexingController extends Controller{
         $parsed_data = array();
         $name = $object->name;
         foreach (explode(' ', $name) as $word) {
-            array_push($parsed_data, (string)$word);
+            array_push($parsed_data, strtolower($word));
         }
         $name = $type->name;
         foreach (explode(' ', $name) as $word) {
-            array_push($parsed_data, (string)$word);
+            array_push($parsed_data, strtolower($word));
         }
         foreach ($properties as $item) {
             $values = (string)$item->value;
             foreach (explode(' ', $values) as $value) {
-                array_push($parsed_data, $value);
+                array_push($parsed_data, strtolower($value));
             }
 
             $subitem = $item->Property()->first();
             $name = (string)$subitem->name;
             foreach (explode(' ', $name) as $subsubitem) {
-                array_push($parsed_data, (string)$subsubitem);
+                array_push($parsed_data, strtolower($subsubitem));
             }
         }
         return $parsed_data;
