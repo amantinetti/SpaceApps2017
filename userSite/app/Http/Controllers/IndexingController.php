0a4b80849f235ec9dc98777427910e12f1988dae
Ezaah <manueltobar13@gmail.com>
2017-04-30 05:15:26 -0300

    Object Parser

--- userSite/app/Http/Controllers/IndexingController.php
+++ userSite/app/Http/Controllers/IndexingController.php
@@ -14,4 +14,32 @@ class IndexingController extends Controller{
 
     }
 
+    public function parse_object($object)
+    {
+        $type = $object->Type()->first();
+        $properties = $object->Properties()->get();
+        $parsed_data = array();
+        $name = $object->name;
+        foreach (explode(' ',$name) as $word) {
+            array_push($parsed_data, (string)$word);
+        }
+        $name = $type->name;
+        foreach (explode(' ',$name) as $word) {
+            array_push($parsed_data, (string)$word);
+        }
+        foreach ($properties as $item) {
+            $values = (string)$item->value;
+            foreach (explode(' ', $values) as $value){
+                array_push($parsed_data, $value);
+            }
+
+            $subitem = $item->Property()->first();
+            $name = (string)$subitem->name;
+            foreach (explode(' ', $name) as $subsubitem) {
+                array_push($parsed_data, (string)$subsubitem);
+            }
+        }
+        return $parsed_data;
+    }
+
 }
