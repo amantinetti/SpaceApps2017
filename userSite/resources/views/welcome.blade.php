3fa7858c5a9e0047b57388ec0b4f6c23a9e7dd28
Rocío Venegas <rocio@Rocios-MacBook-Pro.local>
2017-04-30 10:13:30 -0300

    m

--- userSite/resources/views/welcome.blade.php
+++ userSite/resources/views/welcome.blade.php
@@ -8,8 +8,7 @@
 
             <!-- Logo -->
             <div id="logo">
-                <h1><a href="#">H4m1lt0n</a></h1>
-                <span>Design by TEMPLATED</span>
+                <h1><a href="#">HAMILTON'S</a></h1>
             </div>
 
             <!-- Nav -->
@@ -36,29 +35,25 @@
                 <div class="3u">
                     <section>
                         <a href="#" class="image full"><img src="images/pics01.jpg" width="90" height="150" alt="" /></a>
-                        <p>Quisque dictum. Pellentesque viverra  enim. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
-                        <a href="{{ url('/exoplanets')}}" class="button">Read More</a>
+                        <a href="{{ url('/exoplanets')}}" class="button">Exoplanetas</a>
                     </section>
                 </div>
                 <div class="3u">
                     <section>
                         <a href="#" class="image full"><img src="images/pics11.jpg" width="90" height="150" alt="" /></a>
-                        <p>Pellentesque viverra  enim. Tristique ante ut risus. Quisque dictum. Integer sagittis convallis elementum.</p>
-                        <a href="{{ url('/galaxies')}}" class="button">Read More</a>
+                        <a href="{{ url('/galaxies')}}" class="button">Galaxias</a>
                     </section>
                 </div>
                 <div class="3u">
                     <section>
                         <a href="#" class="image full"><img src="images/pics12.jpg" width="90" height="150" alt="" /></a>
-                        <p>Pellentesque viverra  enim. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
-                        <a href="{{ url('/stars')}}" class="button">Read More</a>
+                        <a href="{{ url('/stars')}}" class="button">Estrellas</a>
                     </section>
                 </div>
                 <div class="3u">
                     <section>
                         <a href="#" class="image full"><img src="images/pics13.jpg" width="90" height="150" alt="" /></a>
-                        <p>Tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis elementum.</p>
-                        <a href="{{ url('/solar_systems')}}" class="button">Read More</a>
+                        <a href="{{ url('/solar_systems')}}" class="button">Sistemas Solares</a>
                     </section>
                 </div>
             </div>
